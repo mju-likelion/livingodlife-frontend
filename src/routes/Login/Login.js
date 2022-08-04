ef11653ffe8008e109dd50482378ff71bd743d38
hyemin <sb_8956@naver.com>
2022-08-04 23:40:59 +0900

    feat: 로그인 api 연결

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,32 +1,70 @@
 import "./Login.scss";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost";
 
 function Login() {
+
+  const loginClient = async (data) => {
+    return await axios.put('/client', data, {validateStatus: () => true,});
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('잘못된 이메일 주소 입니다.').required('이메일 주소를 입력해주세요.'),
+      password: Yup.string()
+      .min(4, '4자 이상 작성해주세요.')
+      .required('비밀번호를 입력해주세요.'),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      loginClient(values);
+    },
+  });
+
   return (
     <div>
       <div className="logo">'GOD생'살기</div>
-      <input
-        className="mail notoSans"
-        type="text"
-        placeholder="이메일 주소"
-      ></input>
-      <input
-        className="pw notoSans"
-        type="password"
-        placeholder="비밀번호"
-      ></input>
-      <br />
-      <button className="login-btn notoSans">로그인</button>
-      <div className="forgot-pw notoSans">계정을 잃어버리셨나요?</div>
+      <form onSubmit={formik.handleSubmit}>
+        <input className="inputForm notoSans" id="email" type="email" placeholder="이메일 주소" {...formik.getFieldProps('email')} />
+        {formik.touched.email && formik.errors.email? (
+      <div className="errorEmail notoSans">{formik.errors.email}</div>
+      ) : formik.touched.email? (
+        <div className="correctEmail notoSans">유효한 이메일 입니다.</div>
+        ):null}
+        <input className="inputForm notoSans"
+          id="password"
+          name="password"
+          type="text"
+          placeholder="비밀번호"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          {...formik.getFieldProps('password')}
+        />
+         {formik.touched.password && formik.errors.password ? (
+      <div className="errorPw notoSans">{formik.errors.password}</div>
+      ) : null}
+       
+        <button className="verify-btn notoSans" type="submit">로그인
+        </button>
+        <div className="forgot-pw notoSans">계정을 잃어버리셨나요?</div>
         <Link
           to="/signup"
           style={{ textDecoration: "none", color: "rgba(129, 129, 129, 1)" }}
         >
           회원가입
         </Link>
+      </form>
     </div>
-  );
+  )
 }
 
 export default Login;
