14cd9d88e38680a43cebeb35f95dee208a194880
park eun bin <8150093@naver.com>
2022-08-05 23:48:01 +0900

    design: header components ui

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,26 +1,3 @@
-// import React from "react";
-// import "./Header.css";
-// import {Row,Col} from 'reactstrap'
-// import { Link } from "react-router-dom";
-
-// function Header() {
-//   return (
-//     <><div id="page-header" className="mb-3">
-//       <Row>
-//         <Col md="6" sm="auto" className="text-center m-auto">
-//           <span className="logo"> 'GOD생' 살기 </span>
-//           <span className="mainBtn"><Link to="/" style={{ textDecoration: 'none' }}>Main</Link></span>
-//           <span className="challengeBtn"><Link to="/challenge" style={{ textDecoration: 'none' }}>Challenge</Link></span>
-//           <span className="routineBtn"><Link to="/routine" style={{ textDecoration: 'none' }}>Routine</Link></span>
-//           <span className="loginBtn"><Link to="/login" style={{ textDecoration: 'none' }}>Login</Link></span>
-
-//         </Col>
-//       </Row>
-//     </div></>
-//   );
-// }
-
-// export default Header;
 import React from "react";
 import "./Header.scss";
 import { Link } from "react-router-dom";
@@ -39,10 +16,10 @@ function Header() {
               <Link to="/routine" >
                 <span>Routine</span>
               </Link>
-              <Link to="/login">
-                <button className="loginBtn">Login</button>
-              </Link>
             </div>
+            <Link to="/login">
+              <button className="loginBtn">Login</button>
+            </Link>
       </div>
   );
 }
