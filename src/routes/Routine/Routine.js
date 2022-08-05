eebb2237f397524bb27e8d5ca7aef4fadf197370
박원호 <36305517+olivyou2@users.noreply.github.com>
2022-08-05 22:29:30 +0900

    update: routine page (#7)
    * 루틴 페이지
    
    * 루틴페이지
    
    * 루틴페이지
    
    * style:피드백적용
    
    * feat: 루틴페이지 (progressbar, DONE)
    
    * fix: progress bar 100에서 멈추기
    
    * fix: opacity 하나빼고 나머지 안눌리는거 수정 및 DONE 삭제
    
    * fix: done card클릭 오류 수정
    
    * style: 주석 및 필요없는 파일 삭제
    
    Co-authored-by: minjik1357 <minji.k1357@gmail.com>

--- src/routes/Routine/Routine.js
+++ src/routes/Routine/Routine.js
@@ -1,14 +1,47 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import './Routine.scss';
 import Header from "../../Components/Header/Header";
+import MorningRT from "./MorningRT.js"
+import NightRT from "./NightRT.js"
+import DailyRT from "./DailyRT.js"
+import running from "../../image/running-icon.png"
 
 
 function Routine() {
-  return (
 
+  const Progress = ({ done }) => (
+    <div className="progress">
+      <div className="progress-done" style={{width:`${done}%`}}>
+        <img src={running} style={{width:'50px', float:'right', margin:'-6px -20px'}}/>
+        <br/>
+        <br/>{ done }% 
+      </div>
+    </div>
+  )
+
+  const [count, setCount] = useState(0)
+  function addCount() {
+    setCount(prevCount => prevCount + 10)
+    if(count >= 100){
+      setCount(100)
+     }
+  }
+
+
+  return (
+    <>
     <Header/>
 
+    <Progress done={count} />
+    
+    <h1 className="routine" onClick={addCount}>Morning Routine</h1>
+      <MorningRT />
+    <h1 className="routine">Night Routine</h1>
+      <NightRT />
+    <h1 className="routine">Daily routine</h1>
+      <DailyRT />
+    </>
   );
 }
 
