b3a1e41cfc37bf9c28cdd27dac04c457e2a9aa27
sb8956 <65844764+sb8956@users.noreply.github.com>
2022-08-07 21:08:55 +0900

    design: login 페이지 Noto Sans 글꼴 적용, input 크기 조정 (#4)
    * design: login 페이지 Noto Sans 글꼴 적용, input 크기 조정
    
    * design: 카카오톡 계정 연동 버튼 삭제
    
    * feat: 로그인 api 연결
    
    * fix: login response flow
    
    * delete: yarn.lock
    
    author: 윤혜민
    
    Co-authored-by: olivyou2 <olivyou2@gmail.com>
    Co-authored-by: 박원호 <36305517+olivyou2@users.noreply.github.com>

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,31 +1,97 @@
-import './Login.scss';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Kakao from '../../image/kakao-icon.png';
+import "./Login.scss";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost";
 
 function Login() {
+  const loginClient = async (data) => {
+    try {
+      const res = await axios.put("/client", data);
+
+      const accessToken = res.data.token;
+      console.log(accessToken);
+    } catch (error) {
+      const err = error.response.data;
+
+      if (err.errorCode) {
+        switch (err.errorCode) {
+          case "WRONG_PASSWORD":
+            alert("잘못된 비밀번호입니다.");
+            break;
+          case "EMAIL_DOES_NOT_EXISTS":
+            alert("가입되지 않은 이메일입니다.");
+            break;
+        }
+      }
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("잘못된 이메일 주소 입니다.")
+        .required("이메일 주소를 입력해주세요."),
+      password: Yup.string()
+        .min(4, "4자 이상 작성해주세요.")
+        .required("비밀번호를 입력해주세요."),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      loginClient(values);
+    },
+  });
 
   return (
     <div>
       <div className="logo">'GOD생'살기</div>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          className="inputForm notoSans"
+          id="email"
+          type="email"
+          placeholder="이메일 주소"
+          {...formik.getFieldProps("email")}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className="errorEmail notoSans">{formik.errors.email}</div>
+        ) : formik.touched.email ? (
+          <div className="correctEmail notoSans">유효한 이메일 입니다.</div>
+        ) : null}
+        <input
+          className="inputForm notoSans"
+          id="password"
+          name="password"
+          type="text"
+          placeholder="비밀번호"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          {...formik.getFieldProps("password")}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div className="errorPw notoSans">{formik.errors.password}</div>
+        ) : null}
 
-      <input className='mail' type="text" placeholder='이메일 주소'></input>
-        <br/>
-      <input className='pw' type="password" placeholder='비밀번호'></input>
-        <br/>
-
-      <button className='login-btn'>로그인</button>
-
-      <div className="hr-sect">또는</div>
-
-      <button className="kakao"><img src={Kakao} style={{ width: '15px', height:'15px'}}/> 카카오톡 계정으로 로그인</button>
-
-      <div className="forgot-pw">계정을 잃어버리셨나요?</div>
-      <a href='./SignUp.js' className="sign-up"><Link to="/signup">회원가입</Link></a>
-
+        <button className="verify-btn notoSans" type="submit">
+          로그인
+        </button>
+        <div className="forgot-pw notoSans">계정을 잃어버리셨나요?</div>
+        <Link
+          to="/signup"
+          style={{ textDecoration: "none", color: "rgba(129, 129, 129, 1)" }}
+        >
+          회원가입
+        </Link>
+      </form>
     </div>
   );
 }
 
 export default Login;
-
